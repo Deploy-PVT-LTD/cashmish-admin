7bbb25801a45cc8fb5d91de3fcbd469db500add2
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2026-02-28 16:03:01 +0500

    update url api

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // export const API_BASE_URL = 'http://localhost:5000/api';
 // export const API_BASE_URL = 'https://cashmish-backend.onrender.com/api';
-export const API_BASE_URL = 'http://cashmish.com/api';
+export const API_BASE_URL = 'http://192.168.1.11:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
