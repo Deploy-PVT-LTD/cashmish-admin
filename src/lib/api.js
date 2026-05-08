b96f078caedf741c08dda1e7b07a3b8ea5d163d4
AriyanNaveed <naveedariyan1@gmail.com>
2026-05-08 12:04:09 +0500

    https added

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // export const API_BASE_URL = 'http://localhost:5000/api';
 // export const API_BASE_URL = 'https://cashmish-backend.onrender.com/api';
-export const API_BASE_URL = 'http://backend.cashmish.com:5000/api';
+export const API_BASE_URL = 'https://backend.cashmish.com:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
