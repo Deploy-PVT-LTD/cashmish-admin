7c662c556cb73ffa1089618681069cb8b9fa3a9e
AriyanNaveed <naveedariyan1@gmail.com>
2026-05-18 18:30:51 +0500

    bhai 2tb added

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -86,6 +86,7 @@ function InventoryForm({ formData, setFormData, onSubmit, isEdit, submitting }) 
               <SelectItem value="256GB">256GB</SelectItem>
               <SelectItem value="512GB">512GB</SelectItem>
               <SelectItem value="1TB">1TB</SelectItem>
+              <SelectItem value="2TB">2TB</SelectItem>
             </SelectContent>
           </Select>
         </div>
